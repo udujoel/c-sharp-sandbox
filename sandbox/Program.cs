a08ffdb4d1bd1de972080225ad7b1ee1762f9c46
Joell <amamihechi.udu@decagon.dev>
2020-01-12 00:01:54 +0100

    Add project files.

---
+++ sandbox/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter x: ");
+            string x = Console.ReadLine();
+
+            Console.WriteLine("Enter y: ");
+            string y = Console.ReadLine();
+
+            string value = IsEqual(x, y) ? "Equal" : "Not Equal";
+
+            Console.WriteLine(value);
+           // Console.WriteLine();
+
+        }
+
+        static bool IsEqual<T>(T x, T y)
+        {
+            Console.WriteLine(typeof(T));
+            return x.Equals(y);
+        }
+    }
+}
